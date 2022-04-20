78dc680fcc377f002a3eddf56b3e43545204033f
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-04-20 17:12:21 +0600

    user added

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const users = [
+    {id:1, name: "Riyad"},
+    {id:2, name: "Shuvo"},
+    {id:3, name: "Sangram"},
+    {id:4, name: "Tazrian"},
+    {id:5, name: "Ozil"}
+]
+
 app.get("/", (req, res) => {
   res.send(`Hello, This is Allah's World!
     Allah Created the whole Universe.`);
@@ -9,6 +17,10 @@ app.get("/", (req, res) => {
 
 app.get('/home', (req, res) => {
     res.send("This is Home 🏡 ")
+})
+
+app.get('/users', (req, res) => {
+    res.send(users)
 })
 
 app.listen(port, () => {
